d13bbea22e88f51d1ecc242ea2b01443f4724f35
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2025-05-17 16:45:48 -0300

    LED RGB project created

---
+++ led-rgb/led-rgb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/i2c.h"
+#include "hardware/pio.h"
+#include "hardware/uart.h"
+
+int main()
+{
+    stdio_init_all();
+    
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
