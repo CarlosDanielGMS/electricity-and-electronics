5d112858fc6692e3d68594e6afe3444a4e06187b
Carlos Daniel <carlosdaniel.pessoal@gmail.com>
2025-05-17 16:48:05 -0300

    Reset button configured

--- led-rgb/led-rgb.c
+++ led-rgb/led-rgb.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
+#include "pico/bootrom.h"
 #include "pico/stdlib.h"
 #include "hardware/spi.h"
 #include "hardware/i2c.h"
 #include "hardware/pio.h"
 #include "hardware/uart.h"
 
+#define IN 0
+#define OUT 1
+#define PRESSED 0
+#define NOT_PRESSED 1
+
+#define RESET_BUTTON_PIN 6
+
+bool resetButtonStatus = NOT_PRESSED;
+
+void initializeComponents();
+void readButtons();
+
 int main()
 {
-    stdio_init_all();
+    initializeComponents();
     
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        readButtons();
+
+        if (resetButtonStatus == PRESSED)
+        {
+            reset_usb_boot(0, 0);
+        }
     }
+}
+
+void initializeComponents()
+{
+    stdio_init_all();
+
+    gpio_init(RESET_BUTTON_PIN);
+    gpio_set_dir(RESET_BUTTON_PIN, IN);
+    gpio_pull_up(RESET_BUTTON_PIN);
+}
+
+void readButtons()
+{
+    resetButtonStatus = gpio_get(RESET_BUTTON_PIN);
 }
